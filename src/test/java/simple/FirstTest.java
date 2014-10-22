c979aa9665ab2d7d6c63c12d1e80a125e419fbf2
SAndrusyk <sergey.andrusyk@gmail.com>
2014-10-22 20:43:16 +0300

    create project
    empty project

---
+++ src/test/java/simple/FirstTest.java
@@ -0,0 +1,24 @@
+package simple;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by bionic on 10/22/14.
+ */
+public class FirstTest {
+
+    @Test
+
+
+    public void firstTest()
+    {
+        System.out.print("new print");
+        Assert.assertTrue(true);
+
+    }
+
+
+
+
+}
