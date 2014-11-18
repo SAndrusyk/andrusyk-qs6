7b36021c227c433203dfdaea064d480ecf144afc
SAndrusyk <sergey.andrusyk@gmail.com>
2014-11-18 21:39:25 +0200

    HW10ForJenkins2

--- src/main/java/utils/ScreenShotMaker.java
+++ src/main/java/utils/ScreenShotMaker.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.remote.Augmenter;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 /**
  * Created by bionic on 11/17/14.
@@ -43,9 +45,12 @@ public class ScreenShotMaker {
         String scrFormat = PropertyLoader.loadProperty("screenshot.format");
         try {
             Augmenter augmenter = new Augmenter();
+            Calendar calendar = Calendar.getInstance();
+            SimpleDateFormat formater = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
             File scrFile = ((TakesScreenshot)augmenter.augment(driver)).getScreenshotAs(OutputType.FILE);
-            FileUtils.copyFile(scrFile, new File(screenShotDirectory + scrName + scrFormat));
-            Log4Test.info("Error screenshot link" + screenShotDirectory + scrName + scrFormat);
+            FileUtils.copyFile(scrFile, new File(screenShotDirectory + scrName + formater.format(calendar.getTime()) + scrFormat));
+
+            Log4Test.info("Error screenshot link: " + PropertyLoader.loadProperty("project.dir")+ screenShotDirectory + scrName + formater.format(calendar.getTime()) + scrFormat);
         }
         catch (Exception e)
         {
