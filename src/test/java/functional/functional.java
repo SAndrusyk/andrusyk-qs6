10065a4cbefd461e2db74d925627498457307841
SAndrusyk <sergey.andrusyk@gmail.com>
2014-11-18 21:54:38 +0200

    HW10ForJenkins4

--- src/test/java/functional/functional.java
+++ src/test/java/functional/functional.java
@@ -23,7 +23,7 @@ import java.util.Calendar;
  */
 public class functional {
 
-    public /*static*/ User user = new User();    // generating user data
+    public static User user = new User();    // generating user data
 
     public static WebDriverWraper driver;
 
